f941b91e9059c5eca63b1617135ca60e8abdcfe8
mykolasenyuk <tecster20@gmail.com>
2022-08-19 00:07:19 +0300

    fixed hero and background

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import s from './Hero.module.scss'
+
+export default function Hero() {
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>
+        branding and web development studio with a social impact
+      </h1>
+    </div>
+  )
+}
