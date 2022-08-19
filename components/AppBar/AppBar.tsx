744dc241b4fa2b687d0204c8b304d771614943fb
yuriiburov <burovyuriireact@gmail.com>
2022-08-19 10:52:18 +0300

    add hover and styles to links

--- components/AppBar/AppBar.tsx
+++ components/AppBar/AppBar.tsx
@@ -1,48 +1,102 @@
-import { Dispatch, FC, SetStateAction, useState } from 'react'
+import { Dispatch, FC, MouseEvent, SetStateAction, useState } from 'react'
+import Sprite from '../Sprite/Sprite'
 import s from './AppBar.module.scss'
 
 interface Props {
-  isActive: boolean
-  setActive: Dispatch<SetStateAction<boolean>>
+	isActive: boolean
+	setActive: Dispatch<SetStateAction<boolean>>
 }
 
 const AppBar: FC<Props> = ({ isActive, setActive }) => {
-  const toggleClass = () => {
-    setActive(!isActive)
-  }
-  return (
-    <header className={s.header}>
-      <div className={s.headerContainer}>
-        <nav className={s.navBar}>
-          <div>
-            <svg className={s.headerLogo}>
-              <use href={'/sprite.svg#icon-logoName'} />
-            </svg>
-          </div>
+	const [isServicesOpen, setServicesOpen] = useState<boolean>(false)
+	const [isAboutUsOpen, setAboutUsOpen] = useState<boolean>(false)
+	const [isContactOpen, setContactOpen] = useState<boolean>(false)
 
-          <button className={s.menuBtn} onClick={toggleClass}>
-            <svg width="48px" height="48px">
-              {isActive ? (
-                <use className={s.iconMenu} href={'/sprite.svg#icon-close'} />
-              ) : (
-                <use className={s.iconMenu} href={'/sprite.svg#icon-menu'} />
-              )}
-            </svg>
-          </button>
+	const toggleClass = () => {
+		setActive(!isActive)
+	}
 
-          <div className={isActive ? s.navMenuOpen : s.navMenu}>
-            <ul className={s.navBarList}>
-              <li className={s.navBarListItem}>
-                <a>SERVICES</a>
-              </li>
-              <li className={s.navBarListItem}>ABOUT US</li>
-              <li className={s.navBarListItem}>CONTACT</li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
-  )
+	const onMenuItemClickHandler = (e: MouseEvent<HTMLLIElement>) => {
+		if (e.currentTarget.textContent === 'SERVICES') {
+			setServicesOpen(!isServicesOpen)
+			setAboutUsOpen(false)
+			setContactOpen(false)
+		}
+		if (e.currentTarget.textContent === 'ABOUT US') {
+			setAboutUsOpen(!isAboutUsOpen)
+			setServicesOpen(false)
+			setContactOpen(false)
+		}
+		if (e.currentTarget.textContent === 'CONTACT') {
+			setContactOpen(!isContactOpen)
+			setAboutUsOpen(false)
+			setServicesOpen(false)
+		}
+	}
+
+	return (
+		<header className={s.header}>
+			<div className={s.headerContainer}>
+				<nav className={s.navBar}>
+					<div>
+						<Sprite id='icon-logoName' width={285} height={48} />
+					</div>
+
+					<button className={s.menuBtn} onClick={toggleClass}>
+						<Sprite
+							id={isActive ? 'icon-close' : 'icon-menu'}
+							width={48}
+							height={48}
+						/>
+					</button>
+
+					<div className={isActive ? s.navMenuOpen : s.navMenu}>
+						<ul className={s.navBarList}>
+							<li
+								style={
+									isServicesOpen
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={s.navBarListItem}
+								onClick={onMenuItemClickHandler}>
+								SERVICES
+							</li>
+							<li
+								style={
+									isAboutUsOpen
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={s.navBarListItem}
+								onClick={onMenuItemClickHandler}>
+								ABOUT US
+							</li>
+							<li
+								style={
+									isContactOpen
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={s.navBarListItem}
+								onClick={onMenuItemClickHandler}>
+								CONTACT
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+	)
 }
 
 export default AppBar
