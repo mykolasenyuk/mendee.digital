62500468d77a68ec697d2ffbca6a5319173d594a
yuriiburov <burovyuriireact@gmail.com>
2022-08-24 21:07:51 +0300

    finished with services

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,38 +8,37 @@ import Services from '../components/Services/Services'
 import s from '../styles/index.module.scss'
 
 const Home = () => {
-  const [isActive, setActive] = useState(false)
-  const [bgColor, setBgColor] = useState<'normal' | 'dark'>('normal')
-  const [open, setOpen] = useState<string>('100%')
-  const [isServicesOpen, setServicesOpen] = useState<boolean>(false)
-  const [isAboutUsOpen, setAboutUsOpen] = useState<boolean>(false)
-  const [isContactOpen, setContactOpen] = useState<boolean>(false)
+	const [isActive, setActive] = useState(false)
+	const [bgColor, setBgColor] = useState<'normal' | 'dark'>('normal')
+	const [open, setOpen] = useState<string>('100%')
+	const [isServicesOpen, setServicesOpen] = useState<boolean>(false)
+	const [isAboutUsOpen, setAboutUsOpen] = useState<boolean>(false)
+	const [isContactOpen, setContactOpen] = useState<boolean>(false)
 
-  return (
-    <div
-      className={`${s.index} ${bgColor === 'dark' ? s['index_darkMode'] : ''}`}
-    >
-      <Background bgColor={bgColor} open={open}>
-        <AppBar
-          isActive={isActive}
-          setActive={setActive}
-          bgColor={bgColor}
-          setBgColor={setBgColor}
-          setOpen={setOpen}
-          isServicesOpen={isServicesOpen}
-          setServicesOpen={setServicesOpen}
-          isAboutUsOpen={isAboutUsOpen}
-          setAboutUsOpen={setAboutUsOpen}
-          isContactOpen={isContactOpen}
-          setContactOpen={setContactOpen}
-        />
-        {!isActive && open === '100%' && <Hero />}
-      </Background>
-      {/* <Services isServicesOpen={isServicesOpen} /> */}
-      <AboutUs isAboutUsOpen={isAboutUsOpen} />
-      <Contact isContactOpen={isContactOpen} />
-    </div>
-  )
+	return (
+		<div
+			className={`${s.index} ${bgColor === 'dark' ? s['index_darkMode'] : ''}`}>
+			<Background bgColor={bgColor} open={open}>
+				<AppBar
+					isActive={isActive}
+					setActive={setActive}
+					bgColor={bgColor}
+					setBgColor={setBgColor}
+					setOpen={setOpen}
+					isServicesOpen={isServicesOpen}
+					setServicesOpen={setServicesOpen}
+					isAboutUsOpen={isAboutUsOpen}
+					setAboutUsOpen={setAboutUsOpen}
+					isContactOpen={isContactOpen}
+					setContactOpen={setContactOpen}
+				/>
+				{!isActive && open === '100%' && <Hero />}
+			</Background>
+			<Services isServicesOpen={isServicesOpen} />
+			<AboutUs isAboutUsOpen={isAboutUsOpen} />
+			<Contact isContactOpen={isContactOpen} />
+		</div>
+	)
 }
 
 export default Home
