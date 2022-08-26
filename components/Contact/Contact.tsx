a1f905971e44b13ec5edafcf32738ac71cb6f287
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2022-08-27 02:00:42 +0300

    added animation fixed styles

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -12,31 +12,44 @@ const Contact: FC<Props> = ({ isContactOpen }) => {
       className={`${s.contact} ${isContactOpen ? s['contact_active'] : ''}`}
     >
       <div className={s.contactContainer}>
+        <h1 className={s.title}>Contact</h1>
         <ul className={s.socialList}>
           <li>
-            <a className={s.socialLink}>
+            <a
+              className={s.socialLink}
+              href="https://www.linkedin.com/company/mendee/"
+            >
               <Sprite id="icon-linkdn" height={30} />
             </a>
           </li>
           <li>
-            <a className={s.socialLink}>
+            <a
+              className={s.socialLink}
+              href="https://twitter.com/mendee_digital"
+            >
               <Sprite id="icon-twitter" height={30} />
             </a>
           </li>
 
           <li>
-            <a className={s.socialLink}>
+            <a
+              className={s.socialLink}
+              href="https://www.facebook.com/mendee.digital/"
+            >
               <Sprite id="icon-facebook" height={30} />
             </a>
           </li>
           <li>
-            <a className={s.socialLink}>
+            <a
+              className={s.socialLink}
+              href="https://www.instagram.com/mendee_digital/"
+            >
               <Sprite id="icon-instagram" height={30} />
             </a>
           </li>
 
           <li>
-            <a className={s.socialLink}>
+            <a className={s.socialLink} href="mailto:hello@mendee.digital">
               <Sprite id="icon-mail" height={30} />
             </a>
           </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,37 +8,38 @@ import Services from '../components/Services/Services'
 import s from '../styles/index.module.scss'
 
 const Home = () => {
-	const [isActive, setActive] = useState(false)
-	const [bgColor, setBgColor] = useState<'normal' | 'dark'>('normal')
-	const [open, setOpen] = useState<boolean>(true)
-	const [isServicesOpen, setServicesOpen] = useState<boolean>(false)
-	const [isAboutUsOpen, setAboutUsOpen] = useState<boolean>(false)
-	const [isContactOpen, setContactOpen] = useState<boolean>(false)
+  const [isActive, setActive] = useState(false)
+  const [bgColor, setBgColor] = useState<'normal' | 'dark'>('normal')
+  const [open, setOpen] = useState<boolean>(true)
+  const [isServicesOpen, setServicesOpen] = useState<boolean>(false)
+  const [isAboutUsOpen, setAboutUsOpen] = useState<boolean>(false)
+  const [isContactOpen, setContactOpen] = useState<boolean>(false)
 
-	return (
-		<div
-			className={`${s.index} ${bgColor === 'dark' ? s['index_darkMode'] : ''}`}>
-			<Background bgColor={bgColor}>
-				<AppBar
-					isActive={isActive}
-					setActive={setActive}
-					bgColor={bgColor}
-					setBgColor={setBgColor}
-					setOpen={setOpen}
-					isServicesOpen={isServicesOpen}
-					setServicesOpen={setServicesOpen}
-					isAboutUsOpen={isAboutUsOpen}
-					setAboutUsOpen={setAboutUsOpen}
-					isContactOpen={isContactOpen}
-					setContactOpen={setContactOpen}
-				/>
-				{!isActive && open && <Hero />}
-			</Background>
-			<Services isServicesOpen={isServicesOpen} />
-			<AboutUs isAboutUsOpen={isAboutUsOpen} />
-			<Contact isContactOpen={isContactOpen} />
-		</div>
-	)
+  return (
+    <div
+      className={`${s.index} ${bgColor === 'dark' ? s['index_darkMode'] : ''}`}
+    >
+      <Background bgColor={bgColor}>
+        <AppBar
+          isActive={isActive}
+          setActive={setActive}
+          bgColor={bgColor}
+          setBgColor={setBgColor}
+          setOpen={setOpen}
+          isServicesOpen={isServicesOpen}
+          setServicesOpen={setServicesOpen}
+          isAboutUsOpen={isAboutUsOpen}
+          setAboutUsOpen={setAboutUsOpen}
+          isContactOpen={isContactOpen}
+          setContactOpen={setContactOpen}
+        />
+        {!isActive && open && <Hero bgColor={bgColor} />}
+      </Background>
+      <Services isServicesOpen={isServicesOpen} />
+      <AboutUs isAboutUsOpen={isAboutUsOpen} />
+      <Contact isContactOpen={isContactOpen} />
+    </div>
+  )
 }
 
 export default Home
