5ca0f943a5ada122b6718aba30dcabeed6e08d00
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2022-08-26 18:35:34 +0300

    fixed styles

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -2,43 +2,43 @@ import { FC, ReactNode } from 'react'
 import s from './Background.module.scss'
 
 interface Props {
-	children: ReactNode
-	bgColor: 'normal' | 'dark' | 'light'
-	open: string
+  children: ReactNode
+  bgColor: 'normal' | 'dark' | 'light'
+  // open: string
 }
 
-const Background: FC<Props> = ({ children, bgColor, open }) => {
-	const testColor = () => {
-		if (bgColor === 'normal') {
-			return s.circles
-		}
-		if (bgColor === 'dark') {
-			return s.circlesDark
-		}
-		return s.circlesLight
-	}
+const Background: FC<Props> = ({ children, bgColor }) => {
+  const testColor = () => {
+    if (bgColor === 'normal') {
+      return s.circles
+    }
+    if (bgColor === 'dark') {
+      return s.circlesDark
+    }
+    return s.circlesLight
+  }
 
-	return (
-		<>
-			<div className={s.area}>
-				<ul className={testColor()} style={{ width: open }}>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-			</div>
-			<div className={s.container}>{children}</div>
-		</>
-	)
+  return (
+    <>
+      <div className={s.area}>
+        <ul className={testColor()}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className={s.container}>{children}</div>
+    </>
+  )
 }
 
 export default Background
