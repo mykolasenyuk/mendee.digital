ae825129fed98962e05c81636a275b04b29e0e6a
yuriiburov <burovyuriireact@gmail.com>
2022-08-26 20:14:33 +0300

    fix burger, burger menu, logo and changed font size on services

--- components/AppBar/AppBar.tsx
+++ components/AppBar/AppBar.tsx
@@ -1,190 +1,223 @@
 import { Dispatch, FC, MouseEvent, SetStateAction, useState } from 'react'
 import Sprite from '../Sprite/Sprite'
 import s from './AppBar.module.scss'
-import 'animate.css'
 
 interface Props {
-  isActive: boolean
-  setActive: Dispatch<SetStateAction<boolean>>
-  bgColor: 'normal' | 'dark'
-  setBgColor: Dispatch<SetStateAction<'normal' | 'dark'>>
-  setOpen: Dispatch<SetStateAction<boolean>>
-  isServicesOpen: boolean
-  setServicesOpen: Dispatch<SetStateAction<boolean>>
-  isAboutUsOpen: boolean
-  setAboutUsOpen: Dispatch<SetStateAction<boolean>>
-  isContactOpen: boolean
-  setContactOpen: Dispatch<SetStateAction<boolean>>
+	isActive: boolean
+	setActive: Dispatch<SetStateAction<boolean>>
+	bgColor: 'normal' | 'dark'
+	setBgColor: Dispatch<SetStateAction<'normal' | 'dark'>>
+	setOpen: Dispatch<SetStateAction<boolean>>
+	isServicesOpen: boolean
+	setServicesOpen: Dispatch<SetStateAction<boolean>>
+	isAboutUsOpen: boolean
+	setAboutUsOpen: Dispatch<SetStateAction<boolean>>
+	isContactOpen: boolean
+	setContactOpen: Dispatch<SetStateAction<boolean>>
 }
 
 const AppBar: FC<Props> = ({
-  isActive,
-  setActive,
-  bgColor,
-  setBgColor,
-  setOpen,
-  isServicesOpen,
-  setServicesOpen,
-  isAboutUsOpen,
-  setAboutUsOpen,
-  isContactOpen,
-  setContactOpen,
+	isActive,
+	setActive,
+	bgColor,
+	setBgColor,
+	setOpen,
+	isServicesOpen,
+	setServicesOpen,
+	isAboutUsOpen,
+	setAboutUsOpen,
+	isContactOpen,
+	setContactOpen,
 }) => {
-  const toggleClass = () => {
-    setActive(!isActive)
-  }
+	const [prevOpenSection, setPrevOpenSection] = useState<
+		'services' | 'about us' | 'contact' | null
+	>(null)
 
-  const onMenuItemClickHandler = (e: MouseEvent<HTMLLIElement>) => {
-    const { textContent } = e.currentTarget
-    if (textContent === 'SERVICES') {
-      setServicesOpen(!isServicesOpen)
-      setAboutUsOpen(false)
-      setContactOpen(false)
-      setBgColor('normal')
-      setOpen(true)
-      setActive(false)
-      if (!isServicesOpen) {
-        setBgColor('normal')
-        setOpen(false)
-      }
-    }
-    if (textContent === 'ABOUT US') {
-      setAboutUsOpen(!isAboutUsOpen)
-      setServicesOpen(false)
-      setContactOpen(false)
-      setBgColor('normal')
-      setOpen(true)
-      setActive(false)
+	const toggleClass = () => {
+		if (isActive) {
+			if (prevOpenSection === 'services') {
+				setServicesOpen(true)
+			}
+			if (prevOpenSection === 'about us') {
+				setAboutUsOpen(true)
+			}
+			if (prevOpenSection === 'contact') {
+				setContactOpen(true)
+			}
+		} else {
+			if (isServicesOpen) {
+				setPrevOpenSection('services')
+				setServicesOpen(false)
+			}
+			if (isAboutUsOpen) {
+				setPrevOpenSection('about us')
+				setAboutUsOpen(false)
+			}
+			if (isContactOpen) {
+				setPrevOpenSection('contact')
+				setContactOpen(false)
+			}
+		}
+		setActive(!isActive)
+	}
 
-      if (!isAboutUsOpen) {
-        setBgColor('normal')
-        setOpen(false)
-      }
-    }
-    if (textContent === 'CONTACT') {
-      setContactOpen(!isContactOpen)
-      setAboutUsOpen(false)
-      setServicesOpen(false)
-      setBgColor('normal')
-      setOpen(true)
-      setActive(false)
-      if (!isContactOpen) {
-        setBgColor('dark')
-        setOpen(false)
-      }
-    }
-  }
+	const onMenuItemClickHandler = (e: MouseEvent<HTMLLIElement>) => {
+		const { textContent } = e.currentTarget
 
-  return (
-    <header className={s.header}>
-      <div className={s.headerContainer}>
-        <nav className={s.navBar}>
-          <div>
-            {bgColor !== 'dark' ? (
-              <svg className={s.headerLogo}>
-                <use href={`/sprite.svg#icon-logoName`} />
-              </svg>
-            ) : (
-              <svg className={s.headerLogo}>
-                <use href={`/sprite.svg#icon-logoName-darkMode`} />
-              </svg>
-            )}
-            {/* <Sprite
+		if (textContent === 'SERVICES') {
+			setServicesOpen(!isServicesOpen)
+			setAboutUsOpen(false)
+			setContactOpen(false)
+			setBgColor('normal')
+			setOpen(true)
+			setActive(false)
+			if (!isServicesOpen) {
+				setBgColor('normal')
+				setOpen(false)
+			}
+		}
+
+		if (textContent === 'ABOUT US') {
+			setAboutUsOpen(!isAboutUsOpen)
+			setServicesOpen(false)
+			setContactOpen(false)
+			setBgColor('normal')
+			setOpen(true)
+			setActive(false)
+
+			if (!isAboutUsOpen) {
+				setBgColor('normal')
+				setOpen(false)
+			}
+		}
+
+		if (textContent === 'CONTACT') {
+			setContactOpen(!isContactOpen)
+			setAboutUsOpen(false)
+			setServicesOpen(false)
+			setBgColor('normal')
+			setOpen(true)
+			setActive(false)
+			if (!isContactOpen) {
+				setBgColor('dark')
+				setOpen(false)
+			}
+		}
+		setPrevOpenSection(null)
+	}
+
+	const logoClickHandler = () => {
+		setActive(false)
+		setOpen(true)
+		setServicesOpen(false)
+		setAboutUsOpen(false)
+		setContactOpen(false)
+	}
+
+	return (
+		<header className={s.header}>
+			<div className={s.headerContainer}>
+				<nav className={s.navBar}>
+					<div style={{ cursor: 'pointer' }} onClick={logoClickHandler}>
+						{bgColor !== 'dark' ? (
+							<svg className={s.headerLogo}>
+								<use href={`/sprite.svg#icon-logoName`} />
+							</svg>
+						) : (
+							<svg className={s.headerLogo}>
+								<use href={`/sprite.svg#icon-logoName-darkMode`} />
+							</svg>
+						)}
+						{/* <Sprite
               id={`icon-logoName${bgColor === 'dark' ? '-darkMode' : ''}`}
               width={285}
               height={48}
             /> */}
-          </div>
+					</div>
 
-          <button
-            className={`${s.menuBtn} ${
-              bgColor === 'dark' ? s['menuBtn_darkMode'] : ''
-            } `}
-            onClick={toggleClass}
-          >
-            <div
-              style={{
-                transition: '350ms cubic-bezier(0.4, 0, 0.2, 1)',
-                rotate: isActive ? '-360deg' : '',
-              }}
-            >
-              <Sprite
-                id={isActive ? 'icon-close' : 'icon-menu'}
-                width={48}
-                height={48}
-                fill={
-                  isActive
-                    ? '#ffa737'
-                    : `${bgColor === 'dark' ? '#fff' : '#314172'}`
-                }
-              />
-            </div>
-          </button>
+					<button
+						className={`${s.menuBtn} ${
+							bgColor === 'dark' ? s['menuBtn_darkMode'] : ''
+						} `}
+						onClick={toggleClass}>
+						<div
+							style={{
+								transition: '350ms cubic-bezier(0.4, 0, 0.2, 1)',
+								rotate: isActive ? '-360deg' : '',
+							}}>
+							<Sprite
+								id={isActive ? 'icon-close' : 'icon-menu'}
+								width={48}
+								height={48}
+								fill={
+									isActive
+										? '#ffa737'
+										: `${bgColor === 'dark' ? '#fff' : '#314172'}`
+								}
+							/>
+						</div>
+					</button>
 
-          <div
-            className={
-              isActive
-                ? `${s.navMenuOpen} ${
-                    bgColor === 'dark' ? s['navMenuOpen_darkMode'] : ''
-                  } `
-                : s.navMenu
-            }
-          >
-            <ul className={s.navBarList}>
-              <li
-                style={
-                  isServicesOpen
-                    ? {
-                        textDecoration: 'underline',
-                        color: '#ffa737',
-                      }
-                    : {}
-                }
-                className={`${s.navBarListItem} ${
-                  bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
-                }`}
-                onClick={onMenuItemClickHandler}
-              >
-                SERVICES
-              </li>
-              <li
-                style={
-                  isAboutUsOpen
-                    ? {
-                        textDecoration: 'underline',
-                        color: '#ffa737',
-                      }
-                    : {}
-                }
-                className={`${s.navBarListItem} ${
-                  bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
-                }`}
-                onClick={onMenuItemClickHandler}
-              >
-                ABOUT US
-              </li>
-              <li
-                style={
-                  isContactOpen
-                    ? {
-                        textDecoration: 'underline',
-                        color: '#ffa737',
-                      }
-                    : {}
-                }
-                className={`${s.navBarListItem} ${
-                  bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
-                }`}
-                onClick={onMenuItemClickHandler}
-              >
-                CONTACT
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
-  )
+					<div
+						className={
+							isActive
+								? `${s.navMenuOpen} ${
+										bgColor === 'dark' ? s['navMenuOpen_darkMode'] : ''
+								  } `
+								: s.navMenu
+						}>
+						<ul className={s.navBarList}>
+							<li
+								style={
+									isServicesOpen || prevOpenSection === 'services'
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={`${s.navBarListItem} ${
+									bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
+								}`}
+								onClick={onMenuItemClickHandler}>
+								SERVICES
+							</li>
+							<li
+								style={
+									isAboutUsOpen || prevOpenSection === 'about us'
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={`${s.navBarListItem} ${
+									bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
+								}`}
+								onClick={onMenuItemClickHandler}>
+								ABOUT US
+							</li>
+							<li
+								style={
+									isContactOpen || prevOpenSection === 'contact'
+										? {
+												textDecoration: 'underline',
+												color: '#ffa737',
+										  }
+										: {}
+								}
+								className={`${s.navBarListItem} ${
+									bgColor === 'dark' ? s['navBarListItem_darkMode'] : ''
+								}`}
+								onClick={onMenuItemClickHandler}>
+								CONTACT
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+	)
 }
 
 export default AppBar
--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,105 +1,31 @@
-import { FC, useState, Dispatch, SetStateAction } from 'react'
-import Sprite from '../Sprite/Sprite'
+import { FC, useState } from 'react'
+import ServicesItem from '../ServicesItem'
 import s from './Services.module.scss'
-
-const leastsData = [
-  { title: 'WEBSITE DESIGN AND CODE', paragraph: 'We love a good code' },
-  { title: 'UX DESIGN', paragraph: 'User friendly and converting' },
-  {
-    title: 'WEBSITE AUDIT',
-    paragraph: 'We will locate your problems and find solutions',
-  },
-  {
-    title: 'BRANDING AND VISUAL IDENTITY',
-    paragraph: 'Logos, colours, TOV, brand guidelines',
-  },
-  {
-    title: 'NFT AND BLOCKCHAIN DEVELOPMENT',
-    paragraph: 'Solana with RUST programming language ',
-  },
-  { title: 'GRAPHIC DESIGN', paragraph: 'From print to all things digital' },
-  {
-    title: 'MOTION GRAPHICS',
-    paragraph:
-      'If a static is worth a thousand words, a motion is worth a million',
-  },
-]
-
-interface ServicesItemProps {
-  isLeastOneOpen: string
-  setLeastOneOpen: Dispatch<SetStateAction<string>>
-  title: string
-  paragraph: string
-}
-
-const ServicesItem: FC<ServicesItemProps> = ({
-  isLeastOneOpen,
-  setLeastOneOpen,
-  title,
-  paragraph,
-}) => {
-  return (
-    <div
-      className={`${s.servicesNavItem} ${
-        isLeastOneOpen === title ? s.servicesNavItemActive : ''
-      }`}
-      onClick={(e) => {
-        const newText = e.currentTarget.childNodes[0].childNodes[0].textContent!
-
-        setLeastOneOpen(newText)
-
-        if (isLeastOneOpen === newText) {
-          setLeastOneOpen('')
-        }
-      }}
-    >
-      <div className={s.servicesNavItemTitle}>
-        <div>{title}</div>
-        <div
-          style={{
-            transition: '250ms',
-            display: 'flex',
-            marginLeft: '15px',
-            rotate: isLeastOneOpen === title ? '-180deg' : '',
-          }}
-        >
-          <Sprite
-            id="icon-rowDown"
-            height={16}
-            width={28}
-            fill={isLeastOneOpen === title ? '#fff' : '#232323'}
-          />
-        </div>
-      </div>
-      <p className={s.servicesNavItemHidden}>{paragraph}</p>
-    </div>
-  )
-}
+import { leastsData } from './utils'
 
 interface Props {
-  isServicesOpen: boolean
+	isServicesOpen: boolean
 }
 
 const Services: FC<Props> = ({ isServicesOpen }) => {
-  const [openLeast, setOpenLeast] = useState<string>('')
+	const [openLeast, setOpenLeast] = useState<string>('')
 
-  return (
-    <aside
-      className={`${s.services} ${isServicesOpen ? s['services_active'] : ''}`}
-    >
-      <nav className={s.servicesNav}>
-        {leastsData.map((data, i) => (
-          <ServicesItem
-            key={i}
-            title={data.title}
-            paragraph={data.paragraph}
-            isLeastOneOpen={openLeast}
-            setLeastOneOpen={setOpenLeast}
-          />
-        ))}
-      </nav>
-    </aside>
-  )
+	return (
+		<aside
+			className={`${s.services} ${isServicesOpen ? s['services_active'] : ''}`}>
+			<nav className={s.servicesNav}>
+				{leastsData.map((data, i) => (
+					<ServicesItem
+						key={i}
+						title={data.title}
+						paragraph={data.paragraph}
+						isLeastOneOpen={openLeast}
+						setLeastOneOpen={setOpenLeast}
+					/>
+				))}
+			</nav>
+		</aside>
+	)
 }
 
 export default Services
